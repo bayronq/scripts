c2471f4862db6e6d3af4d7bbe69d9e839e6a29e6
Bayron in Macbook <bayronq@icloud.com>
2024-02-19 01:50:14 -0600

    Se agregaron directorios adicionales

--- bash/ordercompress.sh
+++ bash/ordercompress.sh
@@ -4,7 +4,8 @@
 origen="/loghist/FF8"
 
 #array
-appserver=('AlertC01' 'AlertD02' 'BPTWA01' 'BPTWB01' 'BPTWC01' 'BPTWD01' 'bcC01' 'bcD01' 'wasCorpC_Logs' 'wasCorpD_Logs')
+diario=('AlertC01' 'AlertD02' 'BPTWA01' 'BPTWB01' 'BPTWC01' 'BPTWD01' 'bcC01' 'bcD01')
+mensual=('wasWSA01' 'wasWSA02' 'wasWSB01' 'wasWSB02' 'wasWSC01' 'wasWSC02' 'wasWSD01' 'wasWSD03' 'WasBM_C01' 'WasBM_D01' 'wasCorpC_Logs' 'wasCorpD_Logs' 'wasA_Logs' 'wasB_Logs' 'wasD_Logs')
 
 # Función para comprimir archivos
 compress_log_file() {
@@ -19,7 +20,6 @@ compress_log_file() {
     local mod_day=$(date -r "$file_path" +"%d")
 
     # Crear estructura de directorios Año/Mes/Día
-    #Crear una carpeta con la fecha de creación si no existe
     local destino="$original_dir/$mod_year/$mod_month/$mod_day"
     if [ ! -d "$destino" ]; then
         mkdir -p "$destino"
@@ -33,11 +33,21 @@ compress_log_file() {
 export -f compress_log_file
 export origen
 
-for i in "${appserver[@]}"; do
+for i in "${diario[@]}"; do
     # Buscar archivos .log y aplicar la función de compresión a cada uno
     origen2=$origen/$i
     echo "Carptea Actual $origen2"
     find $origen2 \( -name 'SystemErr_*.log' -o -name 'SystemOut_*.log' \) -type f -mtime +10 | while read file; do
+        #llama a la función para comprimir y ordenar
+        compress_log_file $file
+    done
+done
+
+for i in "${mensual[@]}"; do
+    # Buscar archivos .log y aplicar la función de compresión a cada uno
+    origen2=$origen/$i
+    echo "Carptea Actual $origen2"
+    find $origen2 \( -name 'SystemErr_*.log' -o -name 'SystemOut_*.log' \) -type f -mtime +31 | while read file; do
         #llama a la función para comprimir y ordenar
         compress_log_file $file
     done
